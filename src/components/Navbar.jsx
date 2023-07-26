ee5fb4a5d285794b3aa602b02a4204ba7136d266
Ivan Verbitskyi <ivan.verbitskyj@gmail.com>
2023-07-26 11:12:23 +0300

    Created Hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
 						window.scrollTo(0, 0);
 					}}
 				>
-					<img src={logo} alt="logo" className="w-20 h-20 object-contain" />
+					<img src={logo} alt="logo" className="w-8 h-8 object-contain" />
 					<p className="text-white text-[18px] font-bold cursor-pointer flex">
 						Blaze &nbsp;<span className="sm:block hidden">| Web-development</span>
 					</p>
